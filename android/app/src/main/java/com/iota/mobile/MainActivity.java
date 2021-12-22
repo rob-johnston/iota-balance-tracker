ea7f776bdff44cb0c25c42c12f2a2e92d7115c7a
Rob Johnston <=>
2021-12-22 15:07:25 +0100

    native dep updates

--- android/app/src/main/java/com/iota/mobile/MainActivity.java
+++ android/app/src/main/java/com/iota/mobile/MainActivity.java
@@ -1,25 +1,13 @@
 package com.iota.mobile;
 
 import android.os.Bundle;
-
-import androidx.activity.result.ActivityResultRegistry;
-import androidx.activity.result.contract.ActivityResultContracts;
-
 import com.getcapacitor.BridgeActivity;
 import com.iotabalanceticker.savecsv.SaveCSVPlugin;
 
 public class MainActivity extends BridgeActivity {
-    private ActivityResultRegistry activityResultRegistry;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         registerPlugin(SaveCSVPlugin.class);
-
-        activityResultRegistry = this.getActivityResultRegistry();
-//        activityResultRegistry.register("key", ActivityResultContracts.CreateDocument()) { (uri) ->
-//            System.out.println("echo");
-//        }
     }
-
-
 }
